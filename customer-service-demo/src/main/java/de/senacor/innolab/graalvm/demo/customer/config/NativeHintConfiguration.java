4a239193992036914e93ba30e145e1b46abedc99
Thomas Engel <thomas.engel@extern.dkbcodefactory.com>
2021-05-07 17:29:56 +0200

    [Spring Boot Demo]: Add Native Hint configuration
        - added configuration for Native Hints in order to fix runtime errors in the native image

---
+++ customer-service-demo/src/main/java/de/senacor/innolab/graalvm/demo/customer/config/NativeHintConfiguration.java
@@ -0,0 +1,24 @@
+package de.senacor.innolab.graalvm.demo.customer.config;
+
+import de.senacor.innolab.graalvm.demo.customer.CustomErrorResponse;
+import de.senacor.innolab.graalvm.demo.customer.controller.openapiMock.model.CustomerDto;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.SynthesizedAnnotation;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ProxyHint;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@NativeHint(
+        types = @TypeHint(types = {
+                CustomerDto.class,
+                CustomErrorResponse.class
+        }),
+        proxies = @ProxyHint(types = {
+                PathVariable.class,
+                SynthesizedAnnotation.class
+        })
+)
+@Configuration(proxyBeanMethods = false)
+public class NativeHintConfiguration {
+}
