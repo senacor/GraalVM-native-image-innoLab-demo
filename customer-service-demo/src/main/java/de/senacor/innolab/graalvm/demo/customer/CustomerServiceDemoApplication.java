5b046d36a9a3e4679d80f246bd1026306e6a70db
Thomas Engel <thomas.engel@extern.dkbcodefactory.com>
2021-05-07 17:26:37 +0200

    [Spring Boot Demo]: Prepare for GraalVM Native Image

--- customer-service-demo/src/main/java/de/senacor/innolab/graalvm/demo/customer/CustomerServiceDemoApplication.java
+++ customer-service-demo/src/main/java/de/senacor/innolab/graalvm/demo/customer/CustomerServiceDemoApplication.java
@@ -3,7 +3,7 @@ package de.senacor.innolab.graalvm.demo.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class CustomerServiceDemoApplication {
 
 	public static void main(String[] args) {
